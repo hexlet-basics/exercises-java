48a1e33d3bb7feca7fd1c41e08befa2cbe0ccbfc
Aleksandr Dmitriev <aleksandr.dmitriev@zalando.de>
2019-07-05 16:32:05 +0200

    delete dates

--- modules/70-dates/20-date/Test.java
+++
@@ -1,7 +0,0 @@
-class Test {
-    public static void main(String[] args) {
-        assert App.getCustomDate(1532435204).equals("18/01/1970");
-        assert App.getCustomDate(532435204).equals("07/01/1970");
-        assert App.getCustomDate(5324352).equals("01/01/1970");
-    }
-}
