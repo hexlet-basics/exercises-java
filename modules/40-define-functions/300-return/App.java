80ed2d6243a33411d76579e2f57715871aa340c7
Sergey Karpuk <sergeykarpuk@yandex.by>
2019-09-27 08:47:55 +0300

    fix soluion 40-define-functions/300-return

--- modules/40-define-functions/300-return/App.java
+++ modules/40-define-functions/300-return/App.java
@@ -1,6 +1,6 @@
 public class App {
     // BEGIN
-    public static int getParentNamesTotalLength(String child) throws Exception{
+    public static int getParentNamesTotalLength(String child) {
         var mother = Functions.parentFor(child, "mother");
         var father = Functions.parentFor(child, "father");
         return mother.length() + father.length();
