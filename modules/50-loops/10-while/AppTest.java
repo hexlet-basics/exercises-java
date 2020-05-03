0942c8c34fff711a8b056ad98ba904f5a26a6342
Kirill Mokevnin <mokevnin@gmail.com>
2020-05-03 15:46:17 -0400

    save

---
+++ modules/50-loops/10-while/AppTest.java
@@ -0,0 +1,37 @@
+package loops.whileLoop;
+
+// import java.io.ByteArrayOutputStream;
+// import java.io.FileDescriptor;
+// import java.io.FileOutputStream;
+// import java.io.PrintStream;
+
+// import static org.assertj.core.api.Assertions.assertThat;
+
+// class Test {
+//     public static void main(String[] args) {
+//         final String expected = "3\n2\n1\nfinished!\n";
+
+//         ByteArrayOutputStream out = new ByteArrayOutputStream();
+//         System.setOut(new PrintStream(out));
+
+//         App.printNumbers(3);
+
+//         final String actual = out.toString();
+
+//         System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+//         System.out.println(actual);
+
+//         assertThat(actual).isEqualTo(expected);
+//     }
+// }
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void reverseTest() {
+        Assert.assertEquals("two", "one");
+    }
+}
