77b8b3b18360b526144917078cc14ae7ee7ff90c
Kirill Mokevnin <mokevnin@gmail.com>
2021-10-26 21:47:47 -0400

    update
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- modules/40-methods-definition/500-define-functions-naming/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    // BEGIN
-    public static void printFormattedBirthday(int day, int month, int year) {
-        System.out.printf("%02d-%02d-%d", day, month, year);
-    }
-    // END
-}
