186df289365040bbd1b9ab428657bd231fb8baea
Maksim Litvinov <maksim.litvinov@hexlet.io>
2021-11-24 14:55:41 +0300

    fix solution

--- modules/45-logic/12-string-comparasion/App.java
+++ modules/45-logic/12-string-comparasion/App.java
@@ -3,8 +3,9 @@ import org.apache.commons.lang3.StringUtils;
 public class App {
     // BEGIN
     public static boolean isPalindrome(String word) {
-        var reversedWord = StringUtils.reverse(word.toLowerCase());
-        return reversedWord.equals(word.toLowerCase());
+        var normalizedWord = word.toLowerCase();
+        var reversedWord = StringUtils.reverse(normalizedWord);
+        return normalizedWord.equals(reversedWord);
     }
     // END
 }
