c2afdfd1a00e55bd0e5bff255dfae71ce62250e9
Sergey Karpuk <sergeykarpuk@yandex.by>
2021-12-28 23:27:29 +0300

    fix(45-logic/25-logical-operators): improve tests

--- modules/45-logic/25-logical-operators/Test.java
+++ modules/45-logic/25-logical-operators/Test.java
@@ -2,8 +2,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Test {
     public static void main(String[] args) {
-        assertThat(App.isLeapYear(2018)).isFalse();
-        assertThat(App.isLeapYear(2017)).isFalse();
         assertThat(App.isLeapYear(2016)).isTrue();
+        assertThat(App.isLeapYear(2000)).isTrue();
+        assertThat(App.isLeapYear(2017)).isFalse();
+        assertThat(App.isLeapYear(2018)).isFalse();
+        assertThat(App.isLeapYear(1900)).isFalse();
     }
 }
