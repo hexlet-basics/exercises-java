bc8ea0a76118a977a970f0af4604574be75b6d66
Sergey Karpuk <sergeykarpuk@yandex.by>
2022-02-17 11:05:03 +0300

    fix(0-conditionals/60-ternary-operator): add empty string case

--- modules/80-conditionals/60-ternary-operator/App.java
+++ modules/80-conditionals/60-ternary-operator/App.java
@@ -3,6 +3,9 @@ import org.apache.commons.lang3.StringUtils;
 public class App {
     // BEGIN
     public static String convertString(String str) {
+        if (str.equals("")) {
+            return "";
+        }
         return Character.isUpperCase(str.charAt(0)) ? str : StringUtils.reverse(str);
     }
     // END
--- modules/80-conditionals/60-ternary-operator/Test.java
+++ modules/80-conditionals/60-ternary-operator/Test.java
@@ -4,5 +4,6 @@ class Test {
     public static void main(String[] args) {
         assertThat(App.convertString("hey")).isEqualTo("yeh");
         assertThat(App.convertString("Hey")).isEqualTo("Hey");
+        assertThat(App.convertString("")).isEqualTo("");
     }
 }
