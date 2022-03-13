577b150196db1de72c70d05b420d41846e84f06c
Kirill Mokevnin <mokevnin@gmail.com>
2022-03-13 14:26:19 -0400

    update content
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- modules/45-logic/12-string-comparasion/App.java
+++ modules/45-logic/12-string-comparasion/App.java
@@ -3,9 +3,8 @@ import org.apache.commons.lang3.StringUtils;
 public class App {
     // BEGIN
     public static boolean isPalindrome(String word) {
-        var normalizedWord = word.toLowerCase();
-        var reversedWord = StringUtils.reverse(normalizedWord);
-        return normalizedWord.equals(reversedWord);
+        var reversedWord = StringUtils.reverse(word);
+        return word.equalsIgnoreCase(reversedWord);
     }
     // END
 }
