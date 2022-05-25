f56ca22e14c33c8978de1c07f8ed5ec46f93e6fb
ssssank <ssssank@gmail.com>
2022-05-25 11:43:51 +0300

    fix typo

--- modules/40-methods-definition/400-method-definition-default-parameters/App.java
+++ modules/40-methods-definition/400-method-definition-default-parameters/App.java
@@ -3,10 +3,10 @@ public class App {
     public static String getHiddenCard(String cardNumber, int starsCount) {
         var visibleDigitsLine = cardNumber.substring(12);
         return "*".repeat(starsCount) + visibleDigitsLine;
-    };
+    }
 
     public static String getHiddenCard(String cardNumber) {
         return App.getHiddenCard(cardNumber, 4);
-    };
+    }
     // END
 }
