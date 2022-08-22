bf516794e44f2464392fa96e521b197fd98cc2a4
Roman Ashikov <rmn.shkv@gmail.com>
2022-08-22 13:59:10 +0700

    (methods-deterministic) add output actual into tests

--- modules/35-methods-using/200-methods-deterministic/Test.java
+++ modules/35-methods-using/200-methods-deterministic/Test.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class Test {
     public static void main(String[] args) {
         IntStream.range(0, 10).forEach(i -> {
+            App.main(null);
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             System.setOut(new PrintStream(out));
 
