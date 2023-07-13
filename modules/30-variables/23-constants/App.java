3577e2c4e99ac0fae82aea5bcf229b4b59809d23
Maksim Litvinov <maksim.litvinov@hexlet.io>
2023-07-13 13:36:08 +0300

    fix const naming

--- modules/30-variables/23-constants/App.java
+++ modules/30-variables/23-constants/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         // BEGIN
-        final var DRAGONS_BORN_COUNT = 3;
+        final var dragonsBornCount = 3;
         // END
     }
 }
