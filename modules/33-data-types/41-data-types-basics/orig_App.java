bc3edd9237e33c5d99f0c429c8fb5726782bc9da
danila.shirkov <danila.shirkov@gmail.com>
2023-09-12 13:14:40 +0300

    rus copy deleted

--- modules/33-data-types/41-data-types-basics/orig_App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        // BEGIN
-        // В Java результатом деления целого числа на целое число всегда является целое число
-        // дробная часть при делении отбрасывается
-        System.out.println(3 / 2);
-        // END
-    }
-}
