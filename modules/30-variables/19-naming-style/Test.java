d808b43e2cf850b26a69fb51c326dd1cb1be2753
unsigable <unsigable@mail.ru>
2024-09-16 19:47:44 +0600

    fixing 22nd java lesson output

--- modules/30-variables/19-naming-style/Test.java
+++ modules/30-variables/19-naming-style/Test.java
@@ -7,14 +7,15 @@ class Test {
     public static void main(String[] args) {
         final var expected = "-1100";
 
-        App.main(null);
-
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
 
         App.main(null);
 
         final var actual = out.toString().trim();
+
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        System.out.println(actual);
 
         assertThat(actual).isEqualTo(expected);
     }
