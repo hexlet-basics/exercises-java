10188d073c9a2f4e887d1e24052b21e1b50cb36a
unsigable <unsigable@mail.ru>
2024-09-24 12:34:25 +0600

    fixing (FileDescriptor.out)

--- modules/30-variables/19-naming-style/Test.java
+++ modules/30-variables/19-naming-style/Test.java
@@ -1,4 +1,6 @@
 import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
 import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
